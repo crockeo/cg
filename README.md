ac6daa9d23480c414e46c01ff9ff53e308ab4a9d
Cerek Hillen <cerekh@gmail.com>
2025-11-08 12:36:07 -0800

    create the architecture

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Building out something like [magit](https://magit.vc/) as a TUI.
 Similar to [gitu](https://github.com/altsem/gitu),
 but laser focused on "fast enough to use in the monolith at $JOB."
 
-## Roadmap
+<!--
+## Features
 
 Look sparse? I started creating after building out some of the basic features.
 
@@ -42,6 +43,41 @@ Look sparse? I started creating after building out some of the basic features.
     - [x] Fix staging a deletion.
 - Cosmetics
     - [ ] Highlight an entire line when hovering, not just the text.
+!-->
+
+## Plan
+
+Keeping notes on what I should do, in what order :)
+
+### Architecture
+
+- **Foreground**
+    - Responsible for all user interaction, input processing + painting the frontend.
+    - Painting thread
+        - Receives signals that it should paint from any source (background job or user input)
+        - Takes mutex on shared state and paints
+    - Input thread
+        - Reads input from stdin as byte strings, and translates it to a logical input
+        - Takes mutex on user state and performs some action based on that input
+        - Can send messages to background job
+- **User State**
+    - Stores information related to the user.
+    - E.g. "what index have I selected"
+    - Or "what partial input have I applied"
+        - This is to capture the idea of an input sequence resulting in one action.
+        - E.g. `c` -> should open up the "commit" menu,
+          and there should be many options inside of the commit menu.
+    - Can be "merged" with a repo state to make sure it is valid w.r.t. the current state of the repo.
+- **Repo State**
+    - Stores information related to the repo.
+    - E.g. the current status, branches, remotes, etc.
+- **Background**
+    - Just a single background thread, which can accept a queue of work.
+    - Each job looks like:
+        - Take job description
+        - Do <some action>
+        - Produce a new repo state
+        - Take mutex on current repo state and swap it out for the new repo state
 
 ## Contributing
 
--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,109 @@ const std = @import("std");
 
 const ui = @import("ui.zig");
 
+const Application = struct {
+    const Self = @This();
+
+    allocator: std.mem.Allocator,
+    background: Background,
+    foreground: Foreground,
+    repo_state: ?RepoState,
+    user_state: UserState,
+
+    pub fn init(allocator: std.mem.Allocator) error{OutOfMemory}!*Self {
+        const self = try allocator.create(Self);
+        self.* = .{
+            .allocator = allocator,
+            .background = Background.init(allocator),
+            .foreground = Foreground.init(allocator),
+            .repo_state = null,
+            .user_state = UserState.init(allocator),
+        };
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.background.deinit();
+        self.foreground.deinit();
+        if (self.repo_state) |*repo_state| {
+            repo_state.deinit();
+        }
+        self.user_state.deinit();
+        self.allocator.destroy(self);
+    }
+};
+
+const Background = struct {
+    const Self = @This();
+
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+};
+
+const Foreground = struct {
+    const Self = @This();
+
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+};
+
+const RepoState = struct {
+    const Self = @This();
+
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+};
+
+const UserState = struct {
+    const Self = @This();
+
+    allocator: std.mem.Allocator,
+
+    pub fn init(allocator: std.mem.Allocator) Self {
+        return .{
+            .allocator = allocator,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        _ = self;
+    }
+};
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
+
+    const app = try Application.init(allocator);
+    defer app.deinit();
 
     var interface = try ui.Interface.init(allocator);
     defer interface.deinit();
